1c5975762031db8206f61ba6e5de1193cc92d1ca
haltonba <haltonba94@gmail.com>
2020-02-17 19:25:37 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,14 @@
+class Application
+
+    def call (env)
+        resp = Rack::Response.new
+        time = Time.now.strftime("%H%M")
+
+        if time.to_i < 1200
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
+    end
+end
